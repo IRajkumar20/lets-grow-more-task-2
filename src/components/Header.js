6a694f73930dd48009aa3e0065419d5cec1e6d2a
YogeshhSahu <87126504+YogeshhSahu@users.noreply.github.com>
2021-08-24 03:09:10 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+    return (
+            <>
+
+            <div className="text-center new">
+              
+            <p>Copyright &copy; 2021</p>
+            </div>
+       
+             </>
+    )
+}
